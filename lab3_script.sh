542577e9258ed325e2128c86bed9350d25154cb0
JazKarit <Jasi5748@colorado.edu>
2019-09-19 09:52:30 -0700

    regex practice file

---
+++ lab3_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Authors : Jaskrit Singh
+# Date: 9/19/2019
+
+#Problem 1 Code:
+echo "Input a file name: "
+read filename
+echo "Input a regular expression: "
+read regX
+grep $regX $filename
+grep -c ...-...-.... regwex_practice.txt
+grep -c @ regwex_practice.txt
+grep -o 303-...-.... regwex_practice.txt
+grep @geocities.com regwex_practice.txt >> email_results.txt
+git add email_results.txt
+git commit -m "Added text file of geocities emails"
+
+
